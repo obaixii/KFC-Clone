0814d59a6ba2b87cbdcd2ca8713b3e6b3a6d5469
Obaid Awan <obaidawan567@gmail.com>
2022-10-17 18:03:10 +0500

    State and Props

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,19 @@ import Footer from "./Components/Footer"
 import { useState } from "react"
 
 function App() {
-  // let a = 90
-  let [num,setNum] = useState(90)
-
-  let handleIncrement = ()=>{
-    if(num>=100) return
-    setNum(num+1)
-
-
-   /* a++
-    // document.getElementById("a").innerHTML = a
-    console.log(a) */
-  }
-  let handleDecrement = ()=>{
-    if(num<=80) return
-    setNum(num-1)
-
-
-   /* a++
-    // document.getElementById("a").innerHTML = a
-    console.log(a) */
-  }
-  return (
+  let [person,setPerson] = useState({
+    name:"obaid",
+    age:21,
+    salary:"50000"
+  })
+  return(
     <>
-    {/* <Header/>
-    <Main/>
-    <Footer/> */}
-    <h5>without curly brackets, content will be rendered as Plain text</h5>
-    <p>25+25</p>
-
-    <h5>With curly brackets, content will be rendered as JS Expression that can be a function, variable or expression</h5>
-    <p>{25+25}</p>
-    <p>{25-25}</p>
-    <p>{25*25}</p>
-    <p>{25/25} written </p>
-    <h3>123123123</h3>
-    <h5>Same case here, a is a variable defined in Js, but when written without &#123; &#125;, it rendered as a Plain Text, but when written with &#123; &#125;, it showed defined value</h5>
-    <p>a</p>
-    <p>{num}</p>
-    <button disabled={num>=100 ? true : false} onClick={handleIncrement}>+</button>
-    <button disabled={num<=80 ? true : false} onClick={handleDecrement}>-</button>
+    <div>
+      <Header person={person}/>
+      {/* <Main/>
+      <Footer/> */}
+    </div>
     </>
-  );
-} 
-
+  )
+  }
 export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,10 @@
-function Header(){
+function Header({person}){
     return(
         <>
         <h2>This is Header</h2>
+        <p>{person.name}</p>
+        <p>{person.age}</p>
+        <p>{person.salary}</p>
         </>
     )
 }
